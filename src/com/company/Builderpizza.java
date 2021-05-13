a8a989f1835a15261bf9b8a46a88e14b050c42dd
davidvilanmiguens <dvilanmiguens@danielcastelao.org>
2021-05-13 14:13:01 +0200

    builder hecho

--- src/com/company/Builderpizza.java
+++ src/com/company/Builderpizza.java
@@ -1,4 +1,39 @@
 package com.company;
 
+import java.io.StringWriter;
+
 public class Builderpizza {
+
+    protected Pizza _pizza;
+    public Pizza built(){
+        return this._pizza;
+    }
+
+    public Builderpizza(){
+        _pizza= new Pizza();
+    }
+
+    public Builderpizza setMasa(String tipoMasa){
+        _pizza.setMasa(tipoMasa);
+        return this;
+    }
+    public Builderpizza setbacon(boolean bacon){
+        _pizza.setBacon(bacon);
+        return this;
+    }
+    public Builderpizza setcebolla(boolean cebolla){
+        _pizza.setCebolla(cebolla);
+        return this;
+    }
+    public Builderpizza setPiña(boolean piña){
+        _pizza.setPiña(piña);
+        return this;
+    }
+    public Builderpizza setRelleno(boolean relleno){
+        _pizza.setRelleno(relleno);
+        return this;
+    }
+
+
+
 }
