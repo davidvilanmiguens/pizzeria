02f95d8810a7dd2659328d9d2ac289e68e7ab431
davidvilanmiguens <dvilanmiguens@danielcastelao.org>
2021-05-13 14:13:42 +0200

    clase main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,62 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+/*Pizza barbacoa=new Pizza();
+barbacoa.setMasa("Masa gruesa");
+barbacoa.setTipo(1);
+barbacoa.setSalsa(true);
+barbacoa.setTipoSalsa("barbacoa");
+barbacoa.setBacon(true);
+barbacoa.setAceitunas(true);
+barbacoa.setPiña(false);
+barbacoa.setJamon(true);
+String mostrarPizza=barbacoa.toString();
+        System.out.println(mostrarPizza);
+Pizza granburger=new Pizza();
+        granburger.setMasa("Masa gruesa");
+        granburger.setTipo(1);
+        granburger.setSalsa(true);
+        granburger.setTipoSalsa("barbacoa");
+        granburger.setBacon(true);
+        granburger.setAceitunas(false);
+        granburger.setPiña(true);
+        granburger.setJamon(true);
+        granburger.setCebolla(true);
+        String mostrarPizza2=barbacoa.toString();
+        System.out.println(mostrarPizza2);
+Pizza jamonqueso =new Pizza();
+        jamonqueso.setMasa("Masa gruesa");
+        jamonqueso.setTipo(1);
+        jamonqueso.setSalsa(true);
+        jamonqueso.setTipoSalsa("barbacoa");
+        jamonqueso.setBacon(true);
+        jamonqueso.setAceitunas(false);
+        jamonqueso.setPiña(false);
+        jamonqueso.setJamon(true);
+        jamonqueso.setCebolla(false);
+        jamonqueso.setExtraQueso(true);
+        String mostrarPizza3=barbacoa.toString();
+        System.out.println(mostrarPizza3);*/
+
+
+        Pizza base = new Builderpizza()
+                .setbacon(true)
+                .setPiña(true)
+                .setMasa("fina")
+                .setcebolla(true)
+                .built();
+
+        Pizza rellena = new Builderpizza()
+                .setbacon(true)
+                .setPiña(true)
+                .setMasa("fina")
+                .setcebolla(true)
+                .setRelleno(true)
+                .built();
+
+        System.out.println("las pizzas"+base+"\n pizza rellena"+rellena);
+
+
     }
 }
